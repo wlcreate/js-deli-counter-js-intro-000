c4c48d1d5d009ca4027467917d3b68ee04ba54f4
Waverley Leung <waverley.nyc@gmail.com>
2020-06-09 23:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function takeANumber(currentLine, name) {
   var place = currentLine.length
   return `Welcome, ${name}. You are number ${place} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine === []) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeliLine.shift()
+  }
+}
